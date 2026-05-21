7ce76e3fc15476aa2b7763b4c22c5644c0f6a7ed
Alemaripartille <mj5kd8yp2k@privaterelay.appleid.com>
2026-05-21 13:49:45 +0200

    Create index.css

---
+++ src/index.css
@@ -0,0 +1,13 @@
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+html {
+  scroll-behavior: smooth;
+}
+
+body {
+  font-family: Arial, sans-serif;
+  background-color: #F8F6F2;
+  color: #2D3748;
+}
