3fb57d9dee0673019e81c7334a54d24406fad03c
Alemaripartille <mj5kd8yp2k@privaterelay.appleid.com>
2026-05-21 11:14:42 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        softBlue: "#7FAFC4",
+        sage: "#A8BFA8",
+        warmWhite: "#F8F6F2",
+        terracotta: "#D98C6B",
+        darkText: "#2D3748",
+      },
+    },
+  },
+  plugins: [],
+}
